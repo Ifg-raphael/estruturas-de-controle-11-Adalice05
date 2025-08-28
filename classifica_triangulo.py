8beeef65b1a068f8df8788f3069c809765a58c3a
Adalice <adalicebastos32@gmail.com>
2025-08-28 09:55:49 -0300

    Implementação finalizada

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,16 @@
-# Sua solução aqui
+a= int(input("Insira o primeiro valor:"))
+b= int(input("Insira o segundo valor:"))
+c= int(input("Insira o terceiro valor:"))
+
+if  a < b+c and b < a+c and c < a+b:
+    if a==b==c:
+        print("equilátero")
+    if a==b!=c or a==c!=b or c==b!=a:
+        print("isóceles")
+    if a!=b!=c:
+        print("escaleno")
+else:
+    print("Não forma triângulo")
+
+
+    
