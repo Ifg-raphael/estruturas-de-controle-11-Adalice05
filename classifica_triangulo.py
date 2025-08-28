715ab2c07fbf61ac31aa21b1cdd94517fb6f09b5
Adalice <adalicebastos32@gmail.com>
2025-08-28 10:08:00 -0300

    Implementação finalizada(teste 2)

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,6 +1,6 @@
-a= int(input("Insira o primeiro valor:"))
-b= int(input("Insira o segundo valor:"))
-c= int(input("Insira o terceiro valor:"))
+a= int(input(" "))
+b= int(input(" "))
+c= int(input(" "))
 
 if  a < b+c and b < a+c and c < a+b:
     if a==b==c:
