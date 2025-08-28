805bc0943b04df15cde3cbf67f7f673293674cac
Adalice <adalicebastos32@gmail.com>
2025-08-28 10:12:03 -0300

    implementação final(teste 3)

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,6 +1,8 @@
-a= int(input(" "))
+'''a= int(input(" "))
 b= int(input(" "))
-c= int(input(" "))
+c= int(input(" "))'''
+
+a, b, c = map(int, input().split())
 
 if  a < b+c and b < a+c and c < a+b:
     if a==b==c:
